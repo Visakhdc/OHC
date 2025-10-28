34e48448abde0ad8b9bac8a4a86521d39e1de663
visakhdc <visakhv2182@gmail.com>
2025-10-28 16:54:52 +0530

    [IMP]:User creation without password

--- care_connector/pydantic_models/product_category.py
+++ care_connector/pydantic_models/product_category.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 class CategoryData(BaseModel):
-    category_name : str
-    parent_category_name: str
+    category_name: str
     x_care_id: str
+    parent_x_care_id: str
--- care_connector/resources/product_category.py
+++ care_connector/resources/product_category.py
@@ -8,20 +8,20 @@ class CategoryUtility:
         try:
             product_category_model = user_env['product.category']
             category_name = category_data.category_name
-            parent_category_name = category_data.parent_category_name
+            parent_x_care_id = category_data.parent_x_care_id
             x_care_id = category_data.x_care_id
             category = product_category_model.search([('x_care_id', '=', x_care_id)], limit=1)
             if not category:
                 category = product_category_model.create({
-                    'name':category_name,
-                    'x_care_id':x_care_id
+                    'name': category_name,
+                    'x_care_id': x_care_id
                 })
 
             category.name = category_name
-            parent_category = product_category_model.search([('name', '=', parent_category_name)], limit=1)
+            parent_category = product_category_model.search([('x_care_id', '=', parent_x_care_id)], limit=1)
             if parent_category:
                 category.parent_id = parent_category.id
             return category
 
         except Exception as e:
-            return {'error': f"Error while creating/updating Category: {str(e)}"}
+            return {str(e)}
